cd332b52d5cac80909248884010490b5ba506de0
Yannick Bijl <yannick.bijl@gmail.com>
2021-03-20 11:42:23 +0100

    Switch from using yaml files to csv files.

--- ParamRunner.py
+++ ParamRunner.py
@@ -1,19 +1,16 @@
 import argparse
+import csv
 import os
 import sys
 
-import yaml
-from yaml.error import YAMLError
 
-
-############ Load YAML file
-def load_yaml(filepath:str) -> dict:
+############ Load config csv file
+def load_config(filepath:str) -> list:
     assert_exist(filepath)
     assert_file(filepath)
     assert_ends(filepath)
-    yamltext = open_text(filepath)
-    yamldict = open_yaml(yamltext)
-    return yamldict
+    config = read_config(filepath)
+    return config
 
 def assert_exist(input_arg:str):
     # Ensure input_arg is a valid existence
@@ -24,30 +21,37 @@ def assert_file(input_arg:str):
     assert os.path.isfile(input_arg)
 
 def assert_ends(input_arg:str):
-    # Ensure input_arg ends with .yaml or .yml 
-    assert not input_arg.endswith('.yaml') or not input_arg.endswith('.yml')
+    # Ensure input_arg file extensions ends with csv
+    assert input_arg.endswith('csv')
 
-def open_text(filepath:str) -> str:
-    with open(filepath) as file:
-        text = file.read()
-    return text
+def read_config(filepath:str) -> list:
+    with open(filepath) as infile:
+        configreader = csv.reader(infile, delimiter=",")
+        config = [row for row in configreader]
+    return config
 
-def open_yaml(yamltext:str) -> dict:
-    try:
-        return yaml.load(yamltext)
-    except yaml.YAMLError as error:
-        print(error)
-        sys.exit()
 
+############ Parse settings from config
+def parse_settings(config:list) -> dict:
+    assert_command(config[0])
+    settings = {}
+    return settings
+
+def assert_command(line):
+    # Ensure that line is only 1 item
+    assert len(line) == 1
+    # Ensure that the line is not empty text
+    assert line[0].strip != ""
 
 
 ############ MAIN
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', dest='yamlfile', type=str, required=True, help="YAML formatted file with user settings.")
+    parser.add_argument('-f', '--file', dest='configfile', type=str, required=True, help="CSV formatted file with user settings.")
     args = parser.parse_args()
 
-    settings = load_yaml(args.yamlfile)
+    config = load_config(args.configfile)
+    settings = parse_settings(config)
 
 if __name__ == "__main__":
     main()
