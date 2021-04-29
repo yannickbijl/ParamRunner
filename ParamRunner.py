fbd3814d914bffbf05a95a34d363727b67f96153
Yannick Bijl <yannick.bijl@gmail.com>
2021-04-29 20:44:33 +0200

    Start implementation feature MeasurePerformance
    - Add argument to activate measuring of performance.
    - Add newly created argument to function call execute_software.

--- ParamRunner.py
+++ ParamRunner.py
@@ -6,10 +6,17 @@ from libs.LoadConfig import load_config
 from libs.ParseSettings import parse_settings
 from libs.ExecuteSoftware import execute_software
 
+def check_positive(value):
+    int_value = int(value)
+    if int_value <= 0:
+        raise argparse.ArgumentTypeError(f"{value} is an invalid int value")
+    return int_value
+
 ############ MAIN
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', dest='configfile', type=str, required=True, help="CSV formatted file with user settings.")
+    parser.add_argument('-m', '--measure', dest='measure', type=check_positive, required=False, default=0, help="")
     args = parser.parse_args()
 
     logger.info("Loading Configfile")
@@ -17,7 +24,10 @@ def main():
     logger.info("Parsing Settings")
     settings = parse_settings(config)
     logger.info("Execute Software")
-    execute_software(settings)
+    execute_software(settings, args.measure)
+    if args.measure > 0:
+        #Further analysis.
+        pass
 
 if __name__ == "__main__":
     logfilename = 'ParamRunner_{:%Y_%m_%d-%H_%M_%S}.log'.format(datetime.now())
