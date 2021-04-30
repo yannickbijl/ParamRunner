6776e9ba0ecebb50cd23720155b149ec1ca3f67c
Yannick Bijl <yannick.bijl@gmail.com>
2021-04-30 21:38:41 +0200

    Implemented the feature MeasurePerformance
    * ParamRunner.py
    - Add checks on the argument 'measure'
    - Create file for measure with header if needed
    - Ensure that log and file with measurements have same timestamp
    - Better formatting of timestamp within the log file
    
    * libs/ExecuteSoftware
    - Run only the command if the argument measure equals None
    - Adjust the command to measure the performance if the argument does not equal None
    - Correct/add type hinting

--- ParamRunner.py
+++ ParamRunner.py
@@ -6,11 +6,31 @@ from libs.LoadConfig import load_config
 from libs.ParseSettings import parse_settings
 from libs.ExecuteSoftware import execute_software
 
+
 def check_positive(value:str) -> int:
     int_value = int(value)
     if int_value <= 0:
         raise argparse.ArgumentTypeError(f"{value} is an invalid int value")
     return int_value
+
+def check_measure_argument(value:int, params:tuple) -> tuple:
+    if value == 0:
+        return None
+    info_header = generate_info_header(params)
+    outfile = create_measurefile(info_header)
+    return (outfile, value)
+
+def generate_info_header(params:tuple) -> str:
+    command_info = "Run," + ",".join(params)
+    performance_info = "Elapsed Time,CPU Usage,Average Memory,Max Memory,Exit Status"
+    return f"{command_info},{performance_info}\n"
+
+def create_measurefile(header) -> str:
+    outfile = f"ParamRunner_{timestamp}.csv"
+    with open(outfile, "w") as file:
+        file.write(header)
+    return outfile
+
 
 ############ MAIN
 def main():
@@ -23,18 +43,17 @@ def main():
     config = load_config(args.configfile)
     logger.info("Parsing Settings")
     settings = parse_settings(config)
+    measure = check_measure_argument(args.measure, settings["params"])
     logger.info("Execute Software")
-    execute_software(settings, args.measure)
-    if args.measure > 0:
-        #Further analysis.
-        pass
+    execute_software(settings, measure)
 
 if __name__ == "__main__":
-    logfilename = 'ParamRunner_{:%Y_%m_%d-%H_%M_%S}.log'.format(datetime.now())
+    timestamp = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+    logfilename = f"ParamRunner_{timestamp}.log"
     logging.basicConfig(filename=logfilename,
                         filemode='a',
                         format='%(asctime)s,%(msecs)d %(levelname)s %(message)s',
-                        datefmt=':%Y_%m_%d-%H:%M:%S',
+                        datefmt='%Y_%m_%d-%H:%M:%S',
                         level=logging.DEBUG)
     logger = logging.getLogger()
     main()
--- libs/ExecuteSoftware.py
+++ libs/ExecuteSoftware.py
@@ -1,19 +1,36 @@
+from datetime import datetime
 import logging
 import os
 
 logger = logging.getLogger(__name__)
 
 ############ Execute commands
-def execute_software(settings:dict):
+def execute_software(settings:dict, measure:tuple=None):
     for paramvalue in settings["values"]:
         run_command = create_command(settings["command"], settings["params"], paramvalue)
-        logger.info(f"Running command with following values {paramvalue}")
-        execute_command(run_command)
+        if measure == None:
+           logger.info(f"Running command with following values {paramvalue}")
+           execute_command(run_command)
+        else:
+            for run in range(measure[1]):                
+                logger.info(f"Running command with following values {paramvalue}")
+                logger.info(f"Performance metrics will be measured amd logged in {measure[0]}")
+                info_line = generate_info_line(run+1,paramvalue)
+                measure_command = adjust_command(run_command, measure[0], info_line)
+                execute_command(measure_command)
 
-def create_command(command:str, params:set, values:tuple) -> str:
+def generate_info_line(run:int, values:tuple) -> str:
+    command_info = f"{run}," + ",".join(values)
+    performance_info = "%E,%P,%K,%M,%x"
+    return f"{command_info},{performance_info}"
+
+def create_command(command:str, params:tuple, values:tuple) -> str:
     for param, value in zip(params, values):
         command = command.replace(f"{'{' + param + '}'}", str(value))
     return command
+
+def adjust_command(original_command:str, outfile:str, info:str) -> str:
+    return f"command time -a -o {outfile} -f {info} {original_command}"
 
 def execute_command(run_command:str):
     os.system(run_command)
