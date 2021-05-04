b1c86c92ddea88c4ca8000c2c3ea29adebb67651
Yannick Bijl <yannick.bijl@gmail.com>
2021-05-04 21:09:25 +0200

    Improved code for package build
    - Improved imports
    - Improved typing and help information
    - Logfile is now only created after arguments are succesfully parsed
    - Ensure timestamp is still accessible for measurefile

--- paramrunner/ParamRunner.py
+++ paramrunner/ParamRunner.py
@@ -1,10 +1,12 @@
 import argparse
-from datetime import datetime
 import logging
 
-from libs.LoadConfig import load_config
-from libs.ParseSettings import parse_settings
-from libs.ExecuteSoftware import execute_software
+from datetime import datetime
+from typing import Union
+
+from .libs.LoadConfig import load_config
+from .libs.ParseSettings import parse_settings
+from .libs.ExecuteSoftware import execute_software
 
 
 def check_positive(value:str) -> int:
@@ -13,11 +15,11 @@ def check_positive(value:str) -> int:
         raise argparse.ArgumentTypeError(f"{value} is an invalid int value")
     return int_value
 
-def check_measure_argument(value:int, params:tuple) -> tuple:
+def check_measure_argument(value:int, params:tuple, timestamp:str) -> Union[None, tuple]:
     if value == 0:
         return None
     info_header = generate_info_header(params)
-    outfile = create_measurefile(info_header)
+    outfile = create_measurefile(info_header, timestamp)
     return (outfile, value)
 
 def generate_info_header(params:tuple) -> str:
@@ -25,7 +27,7 @@ def generate_info_header(params:tuple) -> str:
     performance_info = "Elapsed Time,CPU Usage,Average Memory,Max Memory,Exit Status"
     return f"{command_info},{performance_info}\n"
 
-def create_measurefile(header) -> str:
+def create_measurefile(header, timestamp) -> str:
     outfile = f"ParamRunner_{timestamp}.csv"
     with open(outfile, "w") as file:
         file.write(header)
@@ -34,20 +36,13 @@ def create_measurefile(header) -> str:
 
 ############ MAIN
 def main():
+    # Argument Options
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', dest='configfile', type=str, required=True, help="CSV formatted file with user settings.")
-    parser.add_argument('-m', '--measure', dest='measure', type=check_positive, required=False, default=0, help="")
+    parser.add_argument('-m', '--measure', dest='measure', type=check_positive, required=False, default=0, help="Number of times command with same parameter settings must be run to measure performance.")
     args = parser.parse_args()
 
-    logger.info("Loading Configfile")
-    config = load_config(args.configfile)
-    logger.info("Parsing Settings")
-    settings = parse_settings(config)
-    measure = check_measure_argument(args.measure, settings["params"])
-    logger.info("Execute Software")
-    execute_software(settings, measure)
-
-if __name__ == "__main__":
+    # Creating general variables
     timestamp = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
     logfilename = f"ParamRunner_{timestamp}.log"
     logging.basicConfig(filename=logfilename,
@@ -56,4 +51,14 @@ if __name__ == "__main__":
                         datefmt='%Y_%m_%d-%H:%M:%S',
                         level=logging.DEBUG)
     logger = logging.getLogger()
+
+    logger.info("Loading Configfile")
+    config = load_config(args.configfile)
+    logger.info("Parsing Settings")
+    settings = parse_settings(config)
+    measure = check_measure_argument(args.measure, settings["params"], timestamp)
+    logger.info("Execute Software")
+    execute_software(settings, measure)
+
+if __name__ == "__main__":
     main()
